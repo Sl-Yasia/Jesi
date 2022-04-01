97c125baad6ff1ed7ad67ebaef70f4bb08c33333
Yasiya <85439062+Sl-Yasia@users.noreply.github.com>
2022-04-01 17:54:03 +0530

    ✒️◎ʏᴀsɪʏᴀ◎ ♥️

--- qr.js
+++ qr.js
@@ -14,7 +14,7 @@ const fs = require('fs');
 async function queenJesi () {
     const conn = new WAConnection();
     const Session = new StringSession();  
-    conn.version = [3, 3234, 9]
+    conn.version = [2, 2206, 5]
     conn.logger.level = 'warn';
     conn.regenerateQRIntervalMs = 50000;
 
